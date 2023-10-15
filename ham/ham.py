10a134f3f3295dda5f8f9016d2ac5cda73ea8ebd
Jeremy Ling <jeremyling@Jeremys-Laptop.local>
2023-10-16 00:36:17 +0200

    init random hamiltonian

--- ham/ham.py
+++ ham/ham.py
@@ -2,10 +2,15 @@ import numpy as np
 
 
 def random_hamiltonian(size):
+    
+    """
+    Create a random hamiltonian (h, J)
+    """
+    
     links = dict()
     for i in range (size):
         a = np.random.randint(0,size)
-        w = np.unique(np.random.randint(0,size,a))
+        w = np.unique(np.random.randint(i,size,a))
         j = np.random.standard_cauchy(a)
         links[i] = [(w[p],j[p]) for p in range (len(w)) if a > 0 ]
     return links
