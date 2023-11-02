aaeaf819e3e1a2eea47e12f7f33b15c6a0f52f5f
Jeremy Ling <jeremyling@Jeremys-Laptop.local>
2023-11-03 00:52:50 +0100

    ferro

--- ham/ham.py
+++ ham/ham.py
@@ -21,7 +21,36 @@ def random_hamiltonian(size):
             all_links[k] = c
         else : 
             for i in range (k-1, -1,-1):
-                print('sub ',i )
+                for j in links[i]:
+                    if k == j[0]:
+                        c.append((i, j[1]))
+            all_links[k] = c    
+
+    return links, all_links
+
+def random_ferro_hamiltonian(size):
+    
+    """
+    Create a random hamiltonian (h, J), the first is a unique dictionnary where all links are listed once, the second keeps all links
+    """
+    
+    links = dict()
+    for i in range (size):
+        a = np.random.randint(0,size)
+        l = np.random.randint(i,size,a)
+        l = l[l!=i]
+        w = np.unique(l)
+        j = abs(np.random.standard_normal(w.size))
+        links[i] = [(w[p],j[p]) for p in range (len(w)) if a > 0 ]
+        
+    all_links = dict()
+    
+    for k, v in links.items():
+        c = v.copy()
+        if k == 0 : 
+            all_links[k] = c
+        else : 
+            for i in range (k-1, 0,-1):
                 for j in links[i]:
                     if k == j[0]:
                         c.append((i, j[1]))
